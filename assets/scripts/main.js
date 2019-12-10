97e1a789908d21c9a1f28f489c6d2462b17d2aad
Sia <ashkiani@yahoo.com>
2019-12-09 20:24:39 -0500

    added code for the Search click event

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
     console.log("ready!");
-    var apiKey = "8958a465fb84dd70a7e61cd199ace0f3";
-    var queryURL; //= "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-    const apiCall = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";//{APIKEY}
-    queryURL=apiCall + apiKey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    const apiKey = "8958a465fb84dd70a7e61cd199ace0f3";
+    // const apiCall = "http://api.openweathermap.org/data/2.5/forecast?id=";//524901&APPID=";//{APIKEY}
+    const apiCall = "http://api.openweathermap.org/data/2.5/weather?q=";
+    // https://openweathermap.org/current#name
+    // api.openweathermap.org/data/2.5/weather?q={city name}
+    // api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+
+    $("#searchBtn").click(function (event) {
+        event.preventDefault();
+        var txt = $("#searchText").val();
+
+        txt = txt.trim();
+        if (txt !== "") {
+            queryURL = apiCall + txt + "&APPID=" + apiKey;
+            console.log(queryURL);
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+            });
+        }
+        
     });
-
 });
-// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function (response) {
-//     console.log(response);
-// });
